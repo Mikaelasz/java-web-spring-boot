ba7c6fddc392aac03eb3ed1595cfc9d5cea173ae
Mikaela Soares <miih.arj@gmail.com>
2021-12-05 22:29:10 -0300

    Category memory

---
+++ myfirstproject/src/main/java/com/example/myfirstproject/repository/CategoryRepository.java
@@ -0,0 +1,30 @@
+package com.example.myfirstproject.repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+import com.example.myfirstproject.entities.Category;
+
+@Component
+public class CategoryRepository {
+
+	//Map é uma coleção
+	private Map<Long, Category> map = new HashMap<>();
+	
+	public void save(Category obj) {
+		map.put(obj.getId(), obj);
+		
+	}
+
+	public Category findById(Long id) {
+		return map.get(id);
+	}
+	
+	public List<Category> findAll() {
+		return new ArrayList<Category>(map.values());
+	}
+}
